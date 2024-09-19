3fb4118a91fa2672bb5dce548496b7ec70f06c43
Marco <mbarozzi@gmail.com>
2024-09-19 07:44:25 -0300

    criando primeiros compomentes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
-export default function HomeScreen(){
-return (
-  <div>
-    Ola !
-  </div>
-)
+import Box from "../scr/components/Box";
+import theme from "../scr/theme/theme";
+export default function HomeScreen() {
+  return (
+    <Box
+      tag="main"
+      styleSheet={{
+        fontFamily: theme.typography.fontFamily,
+      }}
+    >
+      Ola !
+    </Box>
+  );
 }
